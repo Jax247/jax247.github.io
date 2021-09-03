06cacc51535fe69831e317dc76caa819be92be7d
Jay  Jeffries <jewan.jeffries@gmail.com>
2021-09-02 23:15:42 -0400

    Visual Overhaul
    Changed background,
    cube color,
    project card rework
    removed self picture replaced with svg.

--- js/main.js
+++ js/main.js
@@ -1,48 +1,118 @@
-generateProjectCards();
+
+
+$(function() {
+  generateProjectCards();
+  VanillaTilt.init(document.querySelectorAll(".card"), {
+
+  });
+});
 
 function generateProjectCards(e) {
-    let cardList = [];
-    let card;
-    let img;
-    let overlay;
-    let icon;
+  let card;
+  let previewImg;
+  let simpleContent;
+  let title;
+  let extraContent;
+  let tags;
+  let newTag;
+  let color;
+  let icon;
+  let projectLinks;
+  let srcLink;
+  let liveLink;
+  let temp1;
+  let temp2
+  let cardlist = [];
   
-    projects.forEach((project) => {
-      // create card container
-      card = document.createElement("div");
-      card.className = "project-card";
-      card.id = project.name;
-  
-      img = document.createElement("img");
-      img.src = project.screenshotpath;
-  
-      overlay = document.createElement("div");
-      overlay.className = "overlay";
-  
-      icon = document.createElement("img");
-      icon.className = "zoom-icon";
-      icon.src = "./assets/pngaaa.com-3712648.png";
-  
-      card.appendChild(img);
-      card.appendChild(overlay);
-      overlay.appendChild(icon);
-  
-      cardList.push(card);
+  projects.forEach((project) => {
+    card = $("<div></div>");
+    card.addClass("card");
+    // card.attr("data-tilt", "")
+    // card.attr("data-tilt-glare", '') 
+    // card.attr("data-tilt-max-glare", "0.1")
+    
+    previewImg = $("<img></img>");
+    previewImg.attr("src", `${project.screenshotpath}`);
+    
+    simpleContent = $("<div></div>");
+    simpleContent.addClass("simple-content");
+    
+    title = $("<h1></h1>");
+    title.text(`${project.name} - ${project.type}`)
+    simpleContent.append(title)
+    simpleContent.append(`${project.description}`)
+    
+    extraContent = $("<div></div>");
+    extraContent.addClass("extra-content");
+    
+    tags = $("<div></div>");
+    tags.addClass("tags");
+    
+    project.tags.forEach((tagName) => {
+      
+      // console.log(projectTags);
+      if (projectTags[tagName.toLowerCase()] !== undefined) {
+        color = projectTags[tagName.toLowerCase()].color;
+        icon = projectTags[tagName.toLowerCase()].icon;
+      } else {
+        color = "#aaa";
+        icon = "/assets/211751_gear_icon.png";
+      }
+      
+      newTag = $("<div></div>");
+      newTag.addClass("tag");
+      newTag.css("background-color", color);
+      
+      newTag.append($(`<img></img>`).attr("src", `${icon}`));
+      newTag.append($(`<p></p>`).text(tagName));
+
+      tags.append(newTag);
     });
-  
-    cardList.forEach((card) => {
-      $("#project-grid").append(card);
-    });
-  }
-  
-$(function () {
-  $(".project-card").click(function () {
-    generateModal(this);
+
+    
+    projectLinks = $("<div></div>");
+    projectLinks.addClass("project-links");
+    
+    temp1 = $("<i></i>");
+    temp1.addClass("fab fa-github");
+    
+    srcLink = $("<a></a>");
+    srcLink.addClass("project-src");
+    srcLink.attr("href", `${project.srcLink}`);
+    srcLink.attr("target", "_blank");
+    srcLink.append(temp1);
+    srcLink.append("Source");
+    
+    temp2 = $("<i></i>");
+    temp2.addClass("fa fa-globe");
+    temp2.attr("aria-hidden", "true");
+    
+    liveLink = $("<a></a>");
+    liveLink.addClass("project-live");
+    liveLink.attr("href", `${project.liveLink}`);
+    liveLink.attr("target", "_blank");
+    liveLink.append(temp2);
+    liveLink.append("Live");
+    
+    projectLinks.append(srcLink);
+    projectLinks.append(liveLink);
+    
+    extraContent.append(tags);
+    extraContent.append(projectLinks);
+
+
+    card.append(previewImg);
+    card.append(simpleContent);
+    card.append(extraContent);
+
+
+    $(".project-container").append(card);
   });
 
-  $(".close").click(function () {
-    closeModal();
-  });
+  
+}
+
+$(function () {
 
   $("#about-link").click(function () {
     $("html, body").animate(
@@ -79,32 +149,30 @@ $(window).click(function (e) {
 });
 
 // Populate modal with data relevant to the project being passed in
-function generateModal(e) {
-  // get relevant divs to change data
-  console.log(e);
+// function generateModal(e) {
+//   // get relevant divs to change data
+//   console.log(e);
 
-  let project = projects.filter((p) => p.name === e.id)[0]; // comes back as array
+//   let project = projects.filter((p) => p.name === e.id)[0]; // comes back as array
 
-  console.log(project);
-  $("#modal-title").html(`${project.name} - ${project.type}`);
-  $("#project-description").html(project.description);
-  $("#project-demo").attr("src", `${project.screenshotpath}`);
-  $(".github-link").attr("href", `${project.srcLink}`)
-  $("#live-link").attr("href", `${project.liveLink}`)
+//   console.log(project);
+//   $("#modal-title").html(`${project.name} - ${project.type}`);
+//   $("#project-description").html(project.description);
+//   $("#project-demo").attr("src", `${project.screenshotpath}`);
+//   $(".github-link").attr("href", `${project.srcLink}`);
+//   $("#live-link").attr("href", `${project.liveLink}`);
 
-  project.tags.forEach((tag) => {
-    $("#tag-list").append(`<li>
-                                <img src="./assets/tick.png" alt="check"/>
-                                ${tag}
-                            </li>`);
-  });
+//   project.tags.forEach((tag) => {
+//     $("#tag-list").append(`<li>
+//                                 <img src="./assets/tick.png" alt="check"/>
+//                                 ${tag}
+//                             </li>`);
+//   });
 
-  $(".modal").css("display", "flex");
-}
+//   $(".modal").css("display", "flex");
+// }
 
-function closeModal() {
-  $("#tag-list").empty();
-  $(".modal").css("display", "none");
-}
-
-
+// function closeModal() {
+//   $("#tag-list").empty();
+//   $(".modal").css("display", "none");
+// }
--- js/projects.js
+++ js/projects.js
@@ -11,18 +11,18 @@ const projects = [
   {
     name: "Portfolio",
     type: "Personal Portfolio",
-    tags: ["HTML", "CSS", "Sass", "JQuery"],
+    tags: ["HTML", "Sass", "JS" ,"JQuery"],
     screenshotpath: "./assets/portfolio-screenshot.png",
-    description: "First iteration of my personal site that you're on now! Made entirely in HTML, SCSS/CSS3, and Jquery/Vanilla Javascript.",
+    description: "First iteration of my personal site that you're on now! Made entirely in HTML, SCSS, and Javascript/Jquery.",
     srcLink: "https://github.com/Jax247/personal-projects/tree/master/portfolio",
     liveLink: 'jax247.github.io',
   },
   {
     name: "Crypt",
     type: "React App Example",
-    tags: ["React", "Next.js", "Chart.js", "CSS", "SCSS", "CoinGecko API", "Axios",],
+    tags: ["React",  "Sass", "Chart.js", "CoinGecko API"],
     screenshotpath: "./assets/crypt-screenshot.png",
-    description: "Crypto Currency Price tracker made with React, Chart.js, and the CoinGecko crypto API. Displays price information for the top 15 coins complete with a price history graph. ",
+    description: "Crypto Currency Price tracker made with React, Chart.js, and the CoinGecko crypto API. Displays price information for the top 15 coins complete with a graph displaying coin prices over various time periods. ",
     srcLink: "https://github.com/Jax247/personal-projects/tree/master/crypt",
     liveLink: '#',
   },
@@ -36,3 +36,32 @@ const projects = [
     liveLink: '#',
   },
 ];
+
+const projectTags = {
+  react: {
+    name: 'React',
+    color: '#2196f3',
+    icon: '/assets/icons8-react-30.png'
+  },
+  html: {
+    name: 'HTML',
+    color: '#f36f21',
+    icon: '/assets/icons8-html-5-30.png'
+  },
+  css: {
+    name: 'CSS3',
+    color: '#0000ff',
+    icon: '/assets/icons8-css3-30.png'
+  },
+  sass: {
+    name: 'Sass',
+    color: '#d34fff',
+    icon: '/assets/icons8-sass-30.png'
+  },
+  js: {
+    name: 'JS',
+    color: '#ffeb3b',
+    icon: '/assets/icons8-javascript-30.png'
+  }
+
+}
