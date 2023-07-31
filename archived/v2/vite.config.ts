4e8faf229ff6c8c29dd573f11f2da6f379160057
Jay  Jeffries <jewan.jeffries@gmail.com>
2023-07-31 11:12:13 -0400

    rebase

--- archived/v2/vite.config.ts
+++ archived/v2/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/jax247.github.io/'
+  // base: '/jax247.github.io/'
 })
